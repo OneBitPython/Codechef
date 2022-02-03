9dcf9550c8ca97af1c3fc819128a8564dd946de5
ananthram <ananthramtennis12@gmail.com>
2022-02-03 10:14:06 +0530

    commited for feb 3rd

---
+++ INOIPRAC/Sequence_Land.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+void print(){cout << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cout << one << " ";
+    print(rest...);
+}
+
+void output(map<int, pair<int,int>>&mp){
+    for(auto itr=mp.begin();itr!=mp.end();++itr){
+        cout << itr->first << " - " << itr->second.first << " " << itr->second.second << endl;
+    }
+}
+
+void read(){};
+template<typename T, typename... Args>
+void read(T& one, Args&... rest){
+    cin >> one;
+    read(rest...);
+}
+
+template<typename T>
+void output(vector<T> &v){
+    for(auto x : v){
+        cout << x << " ";
+    }
+    cout << endl;
+}
+template<typename one,typename two>
+void output(map<one,two> &mp){
+    for(auto itr= mp.begin();itr!=mp.end();++itr){
+        cout << itr->first << " " << itr->second << endl;
+    }
+}
+
+template <typename T>
+void output(set<T>&s){
+    for(auto x : s){
+        cout << x << " ";
+    }
+    cout << endl;
+}
+template <typename T, typename W>
+void output(pair<T, W>&p){
+    cout << p.first << " " << p.second << endl;
+}
+template <typename T, typename W>
+void output(vector<pair<T, W>> &arr){
+    for(auto x : arr){
+        cout << x.first << " " << x.second << endl;
+    }
+}
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<set<int>> arr;
+    vector<set<int>> candidate;
+    set<int>president;
+    int p;
+    cin >> p;
+    for(int i = 0;i<p;++i){
+        int v;
+        cin >> v;
+        president.insert(v);
+    }
+    candidate.pb(president);
+    for(int i = 1;i<n;++i){
+        int q;
+        cin >> q;
+        set<int> values;
+        for(int j = 0;j<q;++j){
+            int v;
+            cin >> v;
+            values.insert(v);
+
+        }
+        arr.pb(values);
+    }
+    auto cnt_intersect = [&](auto one, auto two){
+        vector<int>tmp(300);
+        auto ls = set_intersection(all(one), all(two), tmp.begin());
+
+        return ((ls - tmp.begin()));
+    };
+    int ans = 1;
+    vector<int>visited(n-1,0);
+    while(!candidate.empty()){
+        set<int> curr_p = candidate.front();
+        candidate.erase(candidate.begin());
+        for(int i = 0;i<n-1;++i){
+            if(!visited[i]){
+
+                int cnt = cnt_intersect(curr_p, arr[i]);
+                if(cnt >= k){
+                    ans++;
+                    candidate.pb(arr[i]);
+                    visited[i] = 1;
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    // read(T);
+    while (T--)
+    {
+        solve();
+    }
+}
---
+++ INOIPRAC/Triathlon.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+void print(){cout << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cout << one << " ";
+    print(rest...);
+}
+
+void output(map<int, pair<int,int>>&mp){
+    for(auto itr=mp.begin();itr!=mp.end();++itr){
+        cout << itr->first << " - " << itr->second.first << " " << itr->second.second << endl;
+    }
+}
+
+void read(){};
+template<typename T, typename... Args>
+void read(T& one, Args&... rest){
+    cin >> one;
+    read(rest...);
+}
+
+template<typename T>
+void output(vector<T> &v){
+    for(auto x : v){
+        cout << x << " ";
+    }
+    cout << endl;
+}
+template<typename one,typename two>
+void output(map<one,two> &mp){
+    for(auto itr= mp.begin();itr!=mp.end();++itr){
+        cout << itr->first << " " << itr->second << endl;
+    }
+}
+
+template <typename T>
+void output(set<T>&s){
+    for(auto x : s){
+        cout << x << " ";
+    }
+    cout << endl;
+}
+template <typename T, typename W>
+void output(pair<T, W>&p){
+    cout << p.first << " " << p.second << endl;
+}
+template <typename T, typename W>
+void output(vector<pair<T, W>> &arr){
+    for(auto x : arr){
+        cout << x.first << " " << x.second << endl;
+    }
+}
+struct edge{
+    int a, b, c;
+};
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<edge>arr;
+    for(int i = 0;i<n;++i){
+        edge Edge;
+        int u, v, w;
+        cin >> u >> v >> w;
+        Edge.a = u;
+        Edge.b = v;
+        Edge.c = w;
+        arr.pb(Edge);
+    }
+    sort(all(arr), [&](edge one, edge two){
+        return one.b + one.c > two.b + two.c;
+    });
+    int addon = 0, ans =0 ;
+    for(int i = 0;i<n;++i){
+        ans=max(ans, (addon)+(arr[i].a+arr[i].b+arr[i].c));
+        addon+=(arr[i].a);
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    // read(T);
+    while (T--)
+    {
+        solve();
+    }
+}
