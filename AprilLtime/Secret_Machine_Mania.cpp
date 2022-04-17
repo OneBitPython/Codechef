98276801aded9d9479ff345d50ecefbd4de146e0
ananthram <ananthramtennis12@gmail.com>
2022-04-17 11:57:14 +0530

    commited for april 17th

---
+++ AprilLtime/Secret_Machine_Mania.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void print(){cerr << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cerr << one << " ";
+    print(rest...);
+}
+
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+
+
+
+void solve()
+{
+    int x, c;
+    cin >> x >> c;
+    map<int,int>f;
+    int n= x;
+    for(int i = 2;i<=sqrt(n);++i){
+        if(n%i == 0){
+            f[i] = 1;
+            n/=i;
+            while(n%i == 0){
+                f[i]++;
+                n/=i;
+            }
+        }
+    }
+    if(n>1)f[n]++;
+    int ans = 1;
+    for(auto itr = f.begin();itr!=f.end();++itr){
+        int p = itr->first;
+        int k = itr->second;
+        int go = min(k%c, c-k%c);
+        // dbg(p,k,c);
+        ans*= (int)pow(p,go);
+    }
+    cout << ans << endl;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+}
---
+++ Chef_and_Reversing.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void print(){cerr << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cerr << one << " ";
+    print(rest...);
+}
+
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+
+int dij(int u, int n, vector<vector<pair<int,int>>> &adj){
+    vector<int>costs(n+1, 1e9);
+    costs[u] = 0;
+    priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+    pq.push({0, u});
+    while(!pq.empty()){
+        int u = pq.top().second;
+        pq.pop();
+        for(pair<int,int> v : adj[u]){
+            int curr_cost = costs[v.second];
+            int new_cost = costs[u]+v.first;
+            if(new_cost < curr_cost){
+                costs[v.second] = new_cost;
+                pq.push({new_cost, v.second});
+            }
+        }   
+    }
+    if(costs[n] >= 1e9)return -1;
+    return costs[n];
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int,int>>> adj(n+1);
+    for(int i = 0;i<n;++i){
+        int u, v;
+        cin >> u >> v;
+        adj[u].pb({0, v});
+        adj[v].pb({1, u});
+    }
+    cout << dij(1,n,adj);
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+}
